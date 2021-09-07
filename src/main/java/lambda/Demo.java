6b0ead725eb3c832eb25f97bd6a65e971c097035
Hamza Dabaghi <hamza.dabaghi@gmail.com>
2021-09-07 10:05:22 +0100

    creating the project for lambda expressions

---
+++ src/main/java/lambda/Demo.java
@@ -0,0 +1,18 @@
+package lambda;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        // Functional interface :
+
+        // Consumer :
+
+
+        // Supplier :
+
+        // Function :
+
+        // Predicate :
+    }
+}
