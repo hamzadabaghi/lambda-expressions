c2e5daba18d910a918045001a608d28784081626
Hamza Dabaghi <hamza.dabaghi@gmail.com>
2021-09-07 10:21:34 +0100

    Functional interface using an anonymous inner class

--- src/main/java/lambda/Demo.java
+++ src/main/java/lambda/Demo.java
@@ -4,15 +4,28 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        // Functional interface :
+        /* Functional interface : is an interface with only one abstract method */
 
-        // Consumer :
+        // this is an anonymous inner class
+        Printer p = new Printer() {
+            @Override
+            public void greet(String message) {
+                System.out.println(message);
+            }
+        };
+
+        // test
+        p.greet("hello world");
 
 
-        // Supplier :
 
-        // Function :
+        /* Consumer : */
 
-        // Predicate :
+
+        /* Supplier : */
+
+        /* Function : */
+
+        /* Predicate : */
     }
 }
